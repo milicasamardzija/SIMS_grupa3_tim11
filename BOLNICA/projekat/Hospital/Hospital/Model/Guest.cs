8ae4d11788533163225587d76944ba03e71e7583
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-03-29 12:45:26 +0200

    #dodavanje klase

---
+++ BOLNICA/projekat/Hospital/Hospital/Model/Guest.cs
@@ -0,0 +1,12 @@
+// File:    Guest.cs
+// Author:  Milica
+// Created: Monday, March 29, 2021 12:23:31 PM
+// Purpose: Definition of Class Guest
+
+using System;
+
+public class Guest : User
+{
+   public int guestId;
+
+}
