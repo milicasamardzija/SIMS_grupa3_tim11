7336760dfb3ab3a2e1e0108f73a905aad77e5ad7
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-04-14 00:47:43 +0200

    #izgenerisane potrebne klase

---
+++ BOLNICA/projekat/Hospital/Hospital/RoomInventory.cs
@@ -0,0 +1,14 @@
+// File:    RoomInventory.cs
+// Author:  Milica
+// Created: Tuesday, April 13, 2021 3:17:07 PM
+// Purpose: Definition of Class RoomInventory
+
+using System;
+
+public class RoomInventory
+{
+   public int roomId;
+   public int inventoryId;
+   public int quantity;
+
+}
