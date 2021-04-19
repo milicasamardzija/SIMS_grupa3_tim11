bdf2d1578feadd668ff7acecab9fbb370b1ff034
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-04-19 10:18:20 +0200

    pokusaj1

---
+++ BOLNICA/projekat/Hospital/Hospital/Model/Notice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+   public  class Notice
+    {
+
+    public String notice { get; set; }
+
+    public Notice() { }
+
+    public Notice(String text)
+    {
+        notice = text;
+    }
+    }
+
