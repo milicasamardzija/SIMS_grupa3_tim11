b9a4be873548bc148758595c0f9cc1a94335530f
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-05-03 14:19:59 +0200

    #filtriranje magacina uradjeno

--- BOLNICA/projekat/Hospital/Hospital/Magacin.xaml.cs
+++ BOLNICA/projekat/Hospital/Hospital/Magacin.xaml.cs
@@ -120,5 +120,49 @@ namespace Hospital
                 ListaInventara.ItemsSource = filteredInventory.ToList();
             }
         }
+
+        private void filtriranjeKolicina(object sender, TextChangedEventArgs e)
+        {
+            int kolicina = -1;
+            InventoryFileStorage storage = new InventoryFileStorage();
+            List<Inventory> all = storage.GetAll();
+            if (!KolicinaFiltriranja.Text.Equals(""))
+            {
+               kolicina = Convert.ToInt32(KolicinaFiltriranja.Text);
+            }
+            filteredInventory.Clear();
+
+            if (UslovFiltriranja.SelectedIndex == 0) //>=
+            {
+                foreach (Inventory inventory in all)
+                {
+                    if (inventory.Quantity >= kolicina)
+                    {
+                        filteredInventory.Add(inventory);
+                    } 
+                }
+                ListaInventara.ItemsSource = filteredInventory.ToList();
+                if (KolicinaFiltriranja.Text.Equals(""))
+                {
+                    ListaInventara.ItemsSource = InventoryList;
+                }
+
+            } else if (UslovFiltriranja.SelectedIndex == 1) //<=
+            {
+                foreach (Inventory inventory in all)
+                {
+                    if (inventory.Quantity <= kolicina)
+                    {
+                        filteredInventory.Add(inventory);
+                    }
+                }
+                ListaInventara.ItemsSource = filteredInventory.ToList();
+                if (KolicinaFiltriranja.Text.Equals(""))
+                {
+                    ListaInventara.ItemsSource = InventoryList;
+                }
+            } 
+            
+        }
     }
 }
