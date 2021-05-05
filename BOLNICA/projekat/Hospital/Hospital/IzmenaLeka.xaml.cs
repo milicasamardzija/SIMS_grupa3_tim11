5953fdb7b288eb95bc16f26a989c4c816b788c03
RA57-2018 <bugarski.ra57.2018@uns.ac.rs>
2021-05-05 19:01:33 +0200

    #pocetak_izmene_leka

--- BOLNICA/projekat/Hospital/Hospital/IzmenaLeka.xaml.cs
+++ BOLNICA/projekat/Hospital/Hospital/IzmenaLeka.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.IO;
+using Hospital.Model;
 
 namespace Hospital
 {
@@ -19,14 +22,50 @@ namespace Hospital
     /// </summary>
     public partial class IzmenaLeka : Window
     {
-        public IzmenaLeka()
+
+        public ObservableCollection<Medicine> listMedicine;
+        public Medicine medicine;
+        public int indexSelectedMedicine;
+
+        public IzmenaLeka(ObservableCollection<Medicine> list, Medicine selectedMedicine, int medicineIndex)
         {
             InitializeComponent();
+            this.DataContext = this;
+            listMedicine = list;
+            medicine = selectedMedicine;
+            indexSelectedMedicine = medicineIndex;
+
+            nazivLText.SelectedText = Convert.ToString(selectedMedicine.Name);
+            gramazaLText.SelectedText = Convert.ToString(selectedMedicine.Quantity);
+            vrstaLText.SelectedText = Convert.ToString(selectedMedicine.Type);
+            
+        }
+
+        public void addMedicines()
+        {
+            MedicineFileStorage medicineStorage = new MedicineFileStorage();
+            foreach(Medicine medicine in medicineStorage.GetAll())
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = medicine.Ingredients;
+                combo1.Items.Add(item);
+            }
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MedicineFileStorage medicineStorage = new MedicineFileStorage();
+            medicine.Name = Convert.ToString(nazivLText.Text);
+            medicine.Quantity = Convert.ToDouble(gramazaLText.Text);
+            medicine.Type = Convert.ToString(vrstaLText.Text);
+
+
+            //listMedicine[indexSelectedMedicine] = new Medicine()
         }
     }
 }
