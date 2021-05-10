68d2a11623db7a7bc86d3160497675f47c3fbf82
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-05-10 19:57:30 +0200

    zapoceti hitni

---
+++ BOLNICA/projekat/Hospital/Hospital/Sekretar/HitanPregled.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Hospital.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hospital.Sekretar
+{
+    /// <summary>
+    /// Interaction logic for HitanPregled.xaml
+    /// </summary>
+    public partial class HitanPregled : Window
+    {
+        public HitanPregled()
+        {
+            InitializeComponent();
+            dodajSpecijalizacije();
+        }
+
+        public void dodajSpecijalizacije()
+        {
+
+            specializationCb.ItemsSource = Enum.GetValues(typeof(SpecializationType));
+
+        }
+    }
+}
