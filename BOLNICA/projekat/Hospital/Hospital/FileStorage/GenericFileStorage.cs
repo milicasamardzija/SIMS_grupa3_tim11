41325db61782b109fd804c4211141e9c05883600
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-05-21 14:53:22 +0200

    interfejs  i genericka pocetak

---
+++ BOLNICA/projekat/Hospital/Hospital/FileStorage/GenericFileStorage.cs
@@ -0,0 +1,111 @@
+﻿using Hospital.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.FileStorage
+{
+    public class GenericFileStorage<T> where T: Entity   //????
+
+
+    {
+        private String path;
+
+
+
+        public List<T> GetAll()
+        {
+            List<T> all = new List<T>();
+
+            all = JsonConvert.DeserializeObject<List<T>>(File.ReadAllText(@path));
+
+            return all;
+        }
+
+        public void Save(T newEntity)
+        {
+            List<T> all = GetAll();
+
+            all.Add(newEntity);
+
+            SaveAll(all);
+        }
+
+        public void SaveAll(List<T> entities)
+        {
+            using (StreamWriter file = File.CreateText(@path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, entities);
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            List<T> all = GetAll();
+
+            foreach (T temp in all)
+            {
+                if (temp.Id == entity.Id)
+                {
+                    all.Remove(temp);
+                    break;
+                }
+            }
+            SaveAll(all);
+        }
+
+        public void DeleteById(int id)
+        {
+            List<T> all = GetAll();
+
+            foreach (T entities in all)
+            {
+                if (entities.Id == id)
+                {
+                    all.Remove(entities);
+                    break;
+                }
+            }
+            SaveAll(all);
+        }
+
+        public T FindById(int id)
+        {
+            List<T> all = GetAll();
+            T ret = null;
+
+            foreach (T entity in all)
+            {
+                if (entity.Id == id)
+                {
+                    ret = entity;
+                    break;
+                }
+            }
+
+            return ret;
+        }
+
+        public Boolean ExistsById(int id)
+        {
+            List<T> all = GetAll();
+            Boolean ret = false;
+
+            foreach (T entity in all)
+            {
+                if (entity.Id == id)
+                {
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
+        }
+    }
+}
+
---
+++ BOLNICA/projekat/Hospital/Hospital/Model/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Model
+{
+
+    public class Entity
+    {
+        private int id;
+
+        public Entity() { }
+     public Entity(int id) { this.id = id; }
+
+
+      public int Id
+        {
+            get { return this.id; }
+        }
+
+    }
+
+    
+}
