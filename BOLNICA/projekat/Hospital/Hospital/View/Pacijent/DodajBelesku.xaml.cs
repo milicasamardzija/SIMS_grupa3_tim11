ffbb2332920145a8763e1c2ea830f847f7870138
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2021-05-30 21:06:53 +0200

    biljeskaaa

--- BOLNICA/projekat/Hospital/Hospital/View/Pacijent/DodajBelesku.xaml.cs
+++ BOLNICA/projekat/Hospital/Hospital/View/Pacijent/DodajBelesku.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Hospital.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,13 +21,83 @@ namespace Hospital.View.Pacijent
     /// </summary>
     public partial class DodajBelesku : Window
     {
-        public DodajBelesku()
+        int id;
+        private Anamnesis selectedAnamnesis;
+        private Boolean OnReminder;
+       // private Model.Checkup.Note note;
+        public DodajBelesku(int idP, Anamnesis selected)
         {
             InitializeComponent();
+            id = idP;
+            selectedAnamnesis = selected;
+          //  note = new Model.Checkup.Note();
+            BlackOutDates();
+            startDatePicker.Visibility = Visibility.Hidden;
+            endDatePicker.Visibility = Visibility.Hidden;
+            startDateTextBlock.Visibility = Visibility.Hidden;
+            endDateTextBlock.Visibility = Visibility.Hidden;
+            OnReminder = false;
+
+        }
+
+        private void BlackOutDates()
+        {
+            CalendarDateRange calendar = new CalendarDateRange(DateTime.MinValue, DateTime.Today.AddDays(-1));
+            startDatePicker.BlackoutDates.Add(calendar);
+            endDatePicker.BlackoutDates.Add(calendar);
+            addNote.IsEnabled = false;
+
+
         }
 
         private void addNote_Click(object sender, RoutedEventArgs e)
         {
+
+            String textOfNote = textBox.Text;
+            if (OnReminder == false)
+            {
+                /*note.DescriptionOfNote = textOfNote;
+                note.StartDate = DateTime.MinValue;
+                note.EndDate = DateTime.MinValue;
+
+             /*   if (selectedAnamnesis.NotesForAnamnesis != null)
+                {
+                    selectedAnamnesis.NotesForAnamnesis.Add(note);
+                }
+                else
+                {
+                    List<Note> notes = new List<Note>();
+                    notes.Add(note);
+                    selectedAnamnesis.NotesForAnamnesis = notes;
+                }
+
+                */
+
+
+            }
+            else
+            {
+             /*   note.DescriptionOfNote = textOfNote;
+                note.StartDate = (DateTime)startDatePicker.SelectedDate;
+                note.EndDate = (DateTime)endDatePicker.SelectedDate;
+                note.IsSetReminder = true;
+/*
+                if (selectedAnamnesis.NotesForAnamnesis != null)
+                {
+                    selectedAnamnesis.NotesForAnamnesis.Add(note);
+                }
+                else
+                {
+                    List<Model.Checkup.Note> notes = new List<Model.Checkup.Note>();
+                    notes.Add(note);
+                    selectedAnamnesis.NotesForAnamnesis = notes;
+                }
+*/
+
+
+            }
+
+
 
         }
 
@@ -36,7 +108,7 @@ namespace Hospital.View.Pacijent
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            SetEnabledButtonSubmit();
 
         }
 
@@ -45,13 +117,41 @@ namespace Hospital.View.Pacijent
             CalendarDateRange calendar = new CalendarDateRange(DateTime.MinValue, (DateTime)startDatePicker.SelectedDate);
             endDatePicker.IsEnabled = true;
             endDatePicker.BlackoutDates.Add(calendar);
-          //  SetEnabledButtonSubmit();
+            SetEnabledButtonSubmit();
         }
 
-        private void startMomentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        private void SetEnabledButtonSubmit()
         {
 
+            if (OnReminder == true)
+            {
+                if (startDatePicker.SelectedDate != null && endDatePicker.SelectedDate != null &&
+                    textBox.Text != null)
+                {
+                    addNote.Visibility = Visibility.Visible;
+                    addNote.IsEnabled = true;
+                }
+
+                else
+                {
+                    addNote.IsEnabled = false;
+                }
+            }
+            else
+            {
+                if (textBox.Text != null)
+                {
+                    addNote.IsEnabled = true;
+                }
+                else
+                {
+                    addNote.IsEnabled = false;
+                }
+            }
+
         }
+
 
         private void odustani(object sender, RoutedEventArgs e)
         {
@@ -60,12 +160,18 @@ namespace Hospital.View.Pacijent
 
         private void reminderButton_Click(object sender, RoutedEventArgs e)
         {
+            OnReminder = true;
+            addNote.Visibility = Visibility.Hidden;
+            startDatePicker.Visibility = Visibility.Visible;
+            endDatePicker.Visibility = Visibility.Visible;
+            startDateTextBlock.Visibility = Visibility.Visible;
+            endDateTextBlock.Visibility = Visibility.Visible;
 
         }
 
         private void EndDatePicker_OnSelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
-         
+            SetEnabledButtonSubmit();
         }
     }
 }
