590489e0d21a63c22c8990a2c5a6d94d1fb3bc2a
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-06-07 22:00:28 +0200

    drag and drop lekova uradjen

--- BOLNICA/projekat/Hospital/Hospital/View/Manager/Lekovi/LekoviRevizija/DodavanjeLekaRevizija.xaml.cs
+++ BOLNICA/projekat/Hospital/Hospital/View/Manager/Lekovi/LekoviRevizija/DodavanjeLekaRevizija.xaml.cs
@@ -179,5 +179,57 @@ namespace Hospital
             IngredientsBase.Remove((IngredientDTO) data);
             IngredientsMedicine.Add((IngredientDTO) data);
         }
+
+        private void ZamenskilekoviBaza_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _startPoint = e.GetPosition(null);
+        }
+
+        private void ZamenskilekoviBaza_MouseMove(object sender, MouseEventArgs e)
+        {
+            // No drag operation
+            if (_startPoint == null)
+                return;
+
+            var dg = sender as DataGrid;
+            if (dg == null) return;
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = _startPoint.Value - mousePos;
+            // test for the minimum displacement to begin the drag
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                 Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+
+                // Get the dragged DataGridRow
+                var DataGridRow =
+                    FindAnchestor<DataGridRow>((DependencyObject)e.OriginalSource);
+
+                if (DataGridRow == null)
+                    return;
+                // Find the data behind the DataGridRow
+                var dataTodrop = (MedicineDTO)dg.ItemContainerGenerator.ItemFromContainer(DataGridRow);
+
+                if (dataTodrop == null) return;
+
+                // Initialize the drag & drop operation
+                var dataObj = new DataObject(dataTodrop);
+                dataObj.SetData("DragSource", sender);
+                DragDrop.DoDragDrop(dg, dataObj, DragDropEffects.Copy);
+                _startPoint = null;
+            }
+        }
+
+        private void ZamenskiLekoviLek_Drop(object sender, DragEventArgs e)
+        {
+            var dg = sender as DataGrid;
+            if (dg == null) return;
+            var dgSrc = e.Data.GetData("DragSource") as DataGrid;
+            var data = e.Data.GetData(typeof(MedicineDTO));
+            if (dgSrc == null || data == null) return;
+            MedicinesBase.Remove((MedicineDTO)data);
+            ReplacementMedicine.Add((MedicineDTO)data);
+        }
     }
 }
