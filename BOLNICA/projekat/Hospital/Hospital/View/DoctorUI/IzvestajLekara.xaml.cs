4de93d80882c61536c8304f07fc584c9e2c1bd81
RA57-2018 <bugarski.ra57.2018@uns.ac.rs>
2021-06-08 16:43:37 +0200

    #izvestaj

--- BOLNICA/projekat/Hospital/Hospital/View/DoctorUI/IzvestajLekara.xaml.cs
+++ BOLNICA/projekat/Hospital/Hospital/View/DoctorUI/IzvestajLekara.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using Hospital.Controller;
+using MindFusion.Graphs;
+using MindFusion.Pdf;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Tables;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +26,9 @@ namespace Hospital
     /// </summary>
     public partial class IzvestajLekara : Window
     {
+        CheckupController controller = new CheckupController();
+        PatientFileStorage storagePatient = new PatientFileStorage("./../../../../Hospital/files/storagePatient.json");
+
         public IzvestajLekara()
         {
             InitializeComponent();
@@ -26,13 +36,52 @@ namespace Hospital
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            PrikaziIzvestajLekara showReport = new PrikaziIzvestajLekara();
-            showReport.Show();
-            this.Close();
+            using (PdfDocument document = new PdfDocument())
+            {
+                PdfPage page = document.Pages.Add();
+                Syncfusion.Pdf.Graphics.PdfGraphics graphics = page.Graphics;
+                PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 6);
 
-            CalendarDateRange kalendar = new CalendarDateRange(DateTime.MinValue, DateTime.Today.AddDays(-1));
-            datePicker1.BlackoutDates.Add(kalendar);
-            datePicker2.BlackoutDates.Add(kalendar);
+                font = new PdfStandardFont(PdfFontFamily.Helvetica, 18);
+                graphics.DrawString("Izvestaj o ", font, PdfBrushes.Black, new System.Drawing.PointF(170, 100));
+                graphics.DrawString("pregledu stanja pacijenta", font, PdfBrushes.Black, new System.Drawing.PointF(112, 120));
+
+                font = new PdfStandardFont(PdfFontFamily.Helvetica, 9);
+                StringBuilder stringBuilder = new StringBuilder("");
+                stringBuilder.Append("Prikazan je izvestaj pregleda pacijenta koji se odvio u periodu od ");
+                //graphics.DrawString(stringBuilder.ToString(), font, PdfBrushes.Black, new PointF(28, 180));
+                stringBuilder = new StringBuilder("");
+                stringBuilder.Append(Convert.ToDateTime(datePicker1.Text).ToString("dd.MM.yyyy.")).Append(" do ")
+                    .Append(Convert.ToDateTime(datePicker2.Text).ToString("dd.MM.yyyy."));
+                //graphics.DrawString(stringBuilder.ToString(), font, PdfBrushes.Black, new PointF(28, 192));
+
+                PdfLightTable pdfLightTable = new PdfLightTable();
+                DataTable table = new DataTable();
+                
+                table.Columns.Add("Ime i prezime pacijenta");
+                table.Columns.Add("Datum rodjenja pacijenta");
+                table.Columns.Add("Opis");
+
+                table.Rows.Add(new string[] { "Ime i prezime pacijenta", "Datum rodjenja pacijenta", "Opis" });
+
+                String summary = "";
+
+                foreach(Patient patient in storagePatient.GetAll())
+                {
+                    String name = patient.Name + " " + patient.Surname;
+                    table.Rows.Add(new string[] {
+                        name, patient.BirthdayDate.ToString(),
+                        summary
+                    });
+                }
+                pdfLightTable.DataSource = table;
+                pdfLightTable.Draw(page, new PointF(0, 240));
+                
+                graphics.DrawString(stringBuilder.ToString(), font, PdfBrushes.Black, new PointF(28, 204));
+
+                document.Save("./../../../../Hospital/reports/reportDoctor.pdf");
+                document.Close(true);
+            }
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
