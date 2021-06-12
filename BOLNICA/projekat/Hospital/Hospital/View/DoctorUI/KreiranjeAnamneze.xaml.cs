c6450a827a5f832cef398b288cec66fc137fb5e0
RA57-2018 <bugarski.ra57.2018@uns.ac.rs>
2021-06-12 18:48:44 +0200

    #izmene_u_anamnezi

--- BOLNICA/projekat/Hospital/Hospital/View/DoctorUI/KreiranjeAnamneze.xaml.cs
+++ BOLNICA/projekat/Hospital/Hospital/View/DoctorUI/KreiranjeAnamneze.xaml.cs
@@ -25,6 +25,8 @@ namespace Hospital
     public partial class KreiranjeAnamneze : Window
     {
         public ObservableCollection<CheckupDTO> listCheckup;
+        IAnamnesisFileStorage storageAnamnesis = new AnamnesisFileStorage("./../../../../Hospital/files/anamnesis.json");
+        ObservableCollection<Anamnesis> listAnamnesis = new ObservableCollection<Anamnesis>();
         public CheckupDTO checkup;
         public int index;
 
@@ -57,9 +59,6 @@ namespace Hospital
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            IAnamnesisFileStorage storageAnamnesis = new AnamnesisFileStorage("./../../../../Hospital/files/anamnesis.json");
-            ObservableCollection<Anamnesis> listAnamnesis = new ObservableCollection<Anamnesis>();
-
             Anamnesis newAnamnesis = new Anamnesis(generateIdAnamnesis(), Convert.ToString(textIme), Convert.ToString(textPol.Text),
                 Convert.ToInt16(textDatum.Text), Convert.ToString(textAdresa.Text), Convert.ToString(textBrak.Text),
                 Convert.ToString(textZanimanje.Text), Convert.ToString(textZakljucak.Text));
